8c754e011bba4b64ec540627d9f745878b5eacf9
Sam Bock <smbock42@gmail.com>
2024-01-18 16:25:47 -0800

    Add 404 page and update background image URL

--- src/components/404/content.tsx
+++ src/components/404/content.tsx
@@ -12,7 +12,7 @@ export function HeroSectionFour() {
     <ThemeProvider>
       <SignUpNavBar />
       <header className="h-screen min-h-screen w-screen bg-white">
-        <div className="relative h-screen flex align-center flex-col justify-center text-center bg-cover" style={{backgroundImage: `url('https://images.unsplash.com/photo-1650692201357-3b1b15469952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80')`}}>
+        <div className="relative h-screen flex align-center flex-col justify-center text-center bg-cover" style={{backgroundImage: `url('/404.jpg')`}}>
           <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-gray-900 to-slate-800"></span>
           <Typography
             variant="h1"
@@ -26,8 +26,7 @@ export function HeroSectionFour() {
             color="white"
             className="mb-12 w-full opacity-80 md:w-10/12 lg:w-7/12 mx-auto px-28 z-10"
           >
-            We&apos;re constantly trying to express ourselves and actualize our
-            dreams. If you have the opportunity to play this game.
+            The page you are looking for does not exist.
           </Typography>
           <div className="w-auto mx-auto">
             <div className="flex items-center">
